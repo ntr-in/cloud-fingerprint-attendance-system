a3199975721a5017b3173e2778d95d1699741ab8
ntr-in <antariniit@gmail.com>
2019-10-18 01:17:27 +0530

    Add Modes and code for button

--- sketch-arduino/sketch-arduino.ino
+++ sketch-arduino/sketch-arduino.ino
@@ -1,9 +1,27 @@
+const int push_button = 25;                       // for changing mode on a button press
+                                                  // attendance -> enroll -> delete
+int mode = 1;
+int old_push_val = HIGH;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(push_button, INPUT);
+  Serial.begin(9600);                             // for debugging  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int push_val = digitalRead(push_button);
+
+  if (old_push_val == HIGH && push_val == LOW) {
+    mode++;
+    mode%=3;
+  }
+
+  old_push_val = push_val;
+
+  Serial.println(mode);
+
+  delay(500);
 
 }
