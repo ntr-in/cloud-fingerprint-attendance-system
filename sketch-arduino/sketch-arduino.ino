074f94eb5c236cf35e2d60507b797400bcf4ab44
ntr-in <antariniit@gmail.com>
2019-10-19 19:06:09 +0530

    Prototype Code Arduino

--- sketch-arduino/sketch-arduino.ino
+++ sketch-arduino/sketch-arduino.ino
@@ -1,66 +1,125 @@
 #include <Adafruit_Fingerprint.h>
+#include <RTClib.h>
+#include <Wire.h>
 
 const int push_button = 25;                       // for changing mode on a button press
                                                   // 0. attendance -> 1. enroll -> 2. delete
                                                   
 uint8_t getFingerprintEnroll();
-uint8_t readnumber();
-
+uint8_t readnumber(int m);
+int getFingerprintIDez();
+uint8_t getFingerprintID();
 
 int mode = 0;
 int old_push_val = LOW;
 uint8_t id;
 
+String names[5] = {"antarin index", "bakul index", "antarin middle", "antarin thumb", "antarin small"};
+
+
 SoftwareSerial mySerial(10, 11);                  // For Fingerprint Sensor
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+RTC_DS3231 rtc;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(push_button, INPUT);
   Serial.begin(9600);                             // for debugging
+  delay(500); // wait for console opening
 
+  Wire.begin();
+  delay(500);
+  
   finger.begin(57600);
+  delay(500);
+  
   if (finger.verifyPassword()) {
     Serial.println("Found fingerprint sensor!");
   } else {
     Serial.println("Did not find fingerprint sensor :(");
     while (1) { delay(1); }
   }
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int push_val = digitalRead(push_button);
-
+      
   if (old_push_val == HIGH && push_val == LOW) {
     mode++;
     mode%=3;
   }
 
+  DateTime now = rtc.now();
   old_push_val = push_val;
-  Serial.println("Mode " + String(mode));
 
   if (mode == 1) {
     Serial.println("Ready to enroll a fingerprint!");
     Serial.println("Please type in the ID # (from 1 to 127) you want to save this finger as...");
-    id = readnumber();
+    id = readnumber(1);
     if (id == 0) {// ID #0 not allowed, try again!
        return;
     }
     Serial.print("Enrolling ID #");
-    Serial.println(id);
+    Serial.println(id);     
 
     while (!  getFingerprintEnroll() );
   }
 
+  if (mode == 2) {
+    Serial.println("Please type in the ID # (from 1 to 127) you want to delete...");
+    uint8_t id = readnumber(2);
+    if (id == 0) {// ID #0 not allowed, try again!
+       return;
+    }
+  
+    Serial.print("Deleting ID #");
+    Serial.println(id);
+    
+    deleteFingerprint(id);
+  }
+
+  if (mode == 0) {
+
+    int id = getFingerprintIDez();
+    if (id > 0) {
+      Serial.println("Hello! " + names[id-1]);
+      Serial.print(now.year(), DEC);
+      Serial.print('/');
+      Serial.print(now.month(), DEC);
+      Serial.print('/');
+      Serial.print(now.day(), DEC);
+      Serial.print(" ");
+      Serial.print(now.hour(), DEC);
+      Serial.print(':');
+      Serial.print(now.minute(), DEC);
+      Serial.print(':');
+      Serial.print(now.second(), DEC);
+      Serial.println();
+  
+      Serial.print(" since midnight 1/1/1970 = ");
+      Serial.print(now.unixtime());
+      Serial.print("s = ");
+      Serial.print(now.unixtime() / 86400L);
+      Serial.println("d");
+      
+    }
+    delay(50);            //don't ned to run this at full speed
+  }
+
 }
-
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Fingerprint module
 
-uint8_t readnumber(void) {
+uint8_t readnumber(int m) {
   uint8_t num = 0;
   
   while (num == 0) {
@@ -75,7 +134,7 @@ uint8_t readnumber(void) {
   
       old_push_val = push_val;
   
-      if (mode != 1) {
+      if (mode != m) {
         return 0;
       }
     }
@@ -221,4 +280,105 @@ uint8_t getFingerprintEnroll() {
     Serial.println("Unknown error");
     return p;
   }   
+}
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+  
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+    return p;
+  }   
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+
+  return finger.fingerID;
+}
+
+// returns -1 if failed, otherwise returns ID #
+
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  return finger.fingerID; 
 }
